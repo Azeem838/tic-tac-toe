c8f5620db92b74c7386ea2d52406c5f4df141c70
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-05-12 15:34:58 +0100

    move logics to the logic's file

--- bin/game_logic.rb
+++ bin/game_logic.rb
@@ -1,11 +1,38 @@
 class Players
   attr_reader :name
+  attr_reader :player_num
   def initialize(name, player_num)
     @name = name
     @player_num = player_num
   end
+end
 
-  def sign
-    @player_num == 1 ? 'X' : 'O'
+class Game
+	attr_accessor :df
+   @@sign
+   def sign (t)
+   	@@sign=t
+    @@sign == 1 ? 'X' : 'O'
   end
+ def change(df,move,t)
+ 	@df=df
+ 	@t=t 
+ 	case move
+    when 1 then df.replace_values df[0][0],   sign(t)
+    when 2 then df.replace_values df[1][0],   sign(t)
+    when 3 then df.replace_values df[2][0],   sign(t)
+    when 4 then df.replace_values df[0][1],   sign(t)
+    when 5 then df.replace_values df[1][1],   sign(t)
+    when 6 then df.replace_values df[2][1],   sign(t)
+    when 7 then df.replace_values df[0][2],   sign(t)
+    when 8 then df.replace_values df[1][2],   sign(t)
+    when 9 then df.replace_values df[2][2],   sign(t)
+    end
+ end
+ def start
+ return true
+ end
+
+
+
 end
--- bin/main.rb
+++ bin/main.rb
@@ -8,62 +8,53 @@ puts '******Game Board******'
 df = Daru::DataFrame.new([[1, 4, 7], [2, 5, 8], [3, 6, 9]], order: %i[A B C], index: %i[A B C])
 
 p df
-
+player =[]
 puts 'Player 1: Please Enter your name'
-player1 = Players.new(gets.chomp, 1)
+player[0] = Players.new(gets.chomp, 1)
 
 puts 'Player 2: Please Enter your name'
-player2 = Players.new(gets.chomp, 2)
+player[1] = Players.new(gets.chomp, 2)
 
-name_player = [player1, player2]
+name_player = [player[0].name, player[1].name]
 
 puts '********who is going first?*******'
-puts "Enter 1 for #{name_player[0].name}"
-puts "Enter 2 for #{name_player[1].name}"
+puts "Enter 1 for #{player[0].name}"
+puts "Enter 2 for #{player[1].name}"
 choice = 0
 loop do
   choice = gets.chomp.to_i
   if choice == 1
-    puts " ********Okay!!#{name_player[0].name} will Start*********"
+    puts " ********Okay!!#{player[0].name} will Start*********"
     break
   elsif choice == 2
-    puts "*********Okay!!#{name_player[1].name} will start***********"
+    puts "*********Okay!!#{player[1].name} will start***********"
     break
   else puts 'Please choose either 1, 2'
   end
 end
+new_game=Game.new
+game_on=new_game.start
 
 count = 0
-game_on = true
 t = choice - 1
 
 while game_on
-
-  puts "#{name_player[t].name} Please enter your move"
+  p df
+  puts "#{name_player[t]} Please enter your move"
   move = gets.chomp.to_i
 
   if move >= 1 && move <= 9
-    case move
-    when 1 then df.replace_values df[0][0], name_player[t].sign
-    when 2 then df.replace_values df[1][0], name_player[t].sign
-    when 3 then df.replace_values df[2][0], name_player[t].sign
-    when 4 then df.replace_values df[0][1], name_player[t].sign
-    when 5 then df.replace_values df[1][1], name_player[t].sign
-    when 6 then df.replace_values df[2][1], name_player[t].sign
-    when 7 then df.replace_values df[0][2], name_player[t].sign
-    when 8 then df.replace_values df[1][2], name_player[t].sign
-    when 9 then df.replace_values df[2][2], name_player[t].sign
-    end
+   new_game.change(df,move,t)
     p df
-    puts "#{name_player[t].name} your move is displayed on the board"
+    puts "#{name_player[t]} your move is displayed on the board"
     count += 1
     if move == 8
       game_on = false
-      puts "*******Congratulations #{name_player[t].name} You win"
+      puts "*******Congratulations #{name_player[t]} You win"
     end
     t = (t + 3) % 2
   else
-    puts "invalid move, #{name_player[t].name} please choose again"
+    puts "invalid move, #{name_player[t]} please choose again"
   end
 
   next unless count == 6

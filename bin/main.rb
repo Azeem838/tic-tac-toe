0f613ba365d13be390c8c80d94351f573d888705
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-05-12 05:05:19 +0100

    corrected linters errors

--- bin/main.rb
+++ bin/main.rb
@@ -46,8 +46,8 @@ while game_on
     p df
     puts "#{name_player[t]} your move is displayed on the board"
     count += 1
-    if move==8 
-      game_on=false 
+    if move == 8
+      game_on = false
       puts "*******Congratulations #{name_player[t]} You win"
     end
     t = (t + 3) % 2
